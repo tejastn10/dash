948cf7034eafb77489aef13dc6c425d5aeddbc7e
Tejas Nikhar <tejastn10@gmail.com>
2026-03-21 14:30:00 +0530

    refactor: extract constants to dedicated files

---
+++ src/constants/speedTest.ts
@@ -0,0 +1,8 @@
+export const DOWNLOAD_TEST_SIZES_MB = [1, 5, 10, 25];
+export const MIN_DOWNLOAD_SIZE_MB = 1;
+export const MAX_DOWNLOAD_SIZE_MB = 100;
+
+export const UPLOAD_SIZE_BYTES = 5 * 1024 * 1024;
+
+// crypto.getRandomValues max bytes per call (Web Crypto spec limit)
+export const CRYPTO_CHUNK_SIZE = 65536;
